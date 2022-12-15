48dd81ebae1e15042a7c1ddc050553824f50f856
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2022-12-15 17:40:22 +0100

    Lore ipsum added.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -34,23 +34,24 @@ const Welcome = () => {
     }
 
     return (
-        <div className="flex w-full justify-center items-center">
+        <div className="mt-16 mb-32 flex flex-col gap-y-4 xs:px-4 md:px-16">
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4 space-y-10">
                 <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm_text-5xl text-sky-400 text-gradient py-1">
                         Lotería<br />comunitaria
                     </h1>
-                    <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-                        Subtítulo
+                    <p className="text-left mt-5 text-white font-light md:w-7/12 w-11/12 text-base">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                     </p>
                 </div>
                 <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
                     <form>
                         <input
                             type="number"
-                            placeholder="Cantidad en tickets"
-                            className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+                            placeholder="Cantidad"
+                            className="bg-gray-400 border w-64 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             min={1}
+                            max={10}
                             value={amount}
                             onChange={(e) => setAmount(e.target.value)}
                         />
