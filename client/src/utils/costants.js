d64bf2e1c84a7b493b529cc61b03f64cad8008f7
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2023-01-10 16:50:52 +0100

    Updated constants.js

--- client/src/utils/costants.js
+++ client/src/utils/costants.js
@@ -1,3 +1,3 @@
 export const contractOwner = "0xE210Bee7c101a378A1b3b6a8339D869119De3B4F";
-export const contractAddress = "0x35813D5bAA41bF06540eD735B8aD69360Af733b4";
-export const contractCOST = 3;
+export const contractAddress = "0xF3d4536B0ec82bbA8e87f65C718d4077D82Ef2a0";
+export const contractCOST = 1 / 100;
