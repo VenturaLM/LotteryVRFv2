0aaeefe823f46b0e1e58cbec2163ca8ed41c2417
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2023-01-11 12:57:09 +0100

    File formatted.

--- web3/contracts/Lottery.sol
+++ web3/contracts/Lottery.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
 /**
