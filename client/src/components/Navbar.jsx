9c4b6e26a48ff8a0bcb8aeed65b04beb9a7d8b78
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2023-01-31 09:24:35 +0100

    Minor updates.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
 
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
-            <div className="flex lg:grid-cols-3 md:grid-cols-1 gap-6">
+            <div className="flex lg:grid-cols-3 md:grid-cols-1 gap-6 w-11/12">
                 <div className="md:flex-[0.75] flex-initial justify-center items-center">
                     <img src="../../images/sunglasses_white.png" alt="logo" className="w-32 cursor-pointer" />
                 </div>
 
                 <div className="md:flex-[0.75] sm_text-5xl py-1 font-roboto bg-gradient-to-r bg-clip-text text-transparent from-cyan-300 to-indigo-500 font-light">
-                    Contrato: <a href={contractReference} target="_blank">{contractAddress}</a>
+                    <a href={contractReference} target="_blank">Polygonscan</a>
                 </div>
 
                 <ConnectWallet
